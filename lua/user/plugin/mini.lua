46a1cd46e3775de2d73e5773b15432d9067b7a89
Ariaszzzhc <ariaszzzhc@outlook.com>
2025-01-13 22:39:12 +0800

    add textobject support

--- lua/user/plugin/mini.lua
+++ lua/user/plugin/mini.lua
@@ -224,3 +224,110 @@ add_mini({
     }
   end
 })
+
+add_mini({
+  "echasnovski/mini.ai",
+  config = function()
+    local ai = require("mini.ai")
+
+    ai.setup({
+      n_lines = 500,
+      custom_textobjects = {
+        o = ai.gen_spec.treesitter({
+          a = {
+            "@block.outer",
+            "@conditional.outer",
+            "@loop.outer",
+          },
+          i = {
+            "@block.inner",
+            "@conditional.inner",
+            "@loop.inner",
+          },
+        }),
+        f = ai.gen_spec.treesitter({
+          a = {
+            "@function.outer",
+          },
+          i = {
+            "@function.inner",
+          },
+        }),
+        c = ai.gen_spec.treesitter({
+          a = {
+            "@class.outer",
+          },
+          i = {
+            "@class.inner",
+          },
+        }),
+        t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" },
+        d = { "%f[%d]%d+" }, -- digits
+        e = {                -- Word with case
+          { "%u[%l%d]+%f[^%l%d]", "%f[%S][%l%d]+%f[^%l%d]", "%f[%P][%l%d]+%f[^%l%d]", "^[%l%d]+%f[^%l%d]" },
+          "^().*()$",
+        },
+        u = ai.gen_spec.function_call(),
+        U = ai.gen_spec.function_call({ name_pattern = "[%w_]" }),
+      }
+    })
+
+    local wk = require("which-key")
+
+    local objects = {
+      { " ", desc = "whitespace" },
+      { '"', desc = '" string' },
+      { "'", desc = "' string" },
+      { "(", desc = "() block" },
+      { ")", desc = "() block with ws" },
+      { "<", desc = "<> block" },
+      { ">", desc = "<> block with ws" },
+      { "?", desc = "user prompt" },
+      { "U", desc = "use/call without dot" },
+      { "[", desc = "[] block" },
+      { "]", desc = "[] block with ws" },
+      { "_", desc = "underscore" },
+      { "`", desc = "` string" },
+      { "a", desc = "argument" },
+      { "b", desc = ")]} block" },
+      { "c", desc = "class" },
+      { "d", desc = "digit(s)" },
+      { "e", desc = "CamelCase / snake_case" },
+      { "f", desc = "function" },
+      { "g", desc = "entire file" },
+      { "i", desc = "indent" },
+      { "o", desc = "block, conditional, loop" },
+      { "q", desc = "quote `\"'" },
+      { "t", desc = "tag" },
+      { "u", desc = "use/call" },
+      { "{", desc = "{} block" },
+      { "}", desc = "{} with ws" },
+    }
+
+    local ret = { mode = { "o", "x" } }
+    local mappings = {
+      around = "a",
+      inside = "i",
+      around_next = "an",
+      inside_next = "in",
+      around_last = "al",
+      inside_last = "il",
+    }
+    mappings.goto_left = nil
+    mappings.goto_right = nil
+
+    for name, prefix in pairs(mappings) do
+      name = name:gsub("^around_", ""):gsub("^inside_", "")
+      ret[#ret + 1] = { prefix, group = name }
+      for _, obj in ipairs(objects) do
+        local desc = obj.desc
+        if prefix:sub(1, 1) == "i" then
+          desc = desc:gsub(" with ws", "")
+        end
+        ret[#ret + 1] = { prefix .. obj[1], desc = obj.desc }
+      end
+    end
+
+    wk.add(ret, { notify = false })
+  end,
+})
--- lua/user/plugin/treesitter.lua
+++ lua/user/plugin/treesitter.lua
@@ -2,17 +2,29 @@ local M = {
   "nvim-treesitter/nvim-treesitter",
   event = { "BufReadPost", "BufNewFile" },
   build = ":TSUpdate",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects"
+  }
 }
 
 function M.config()
-  require("nvim-treesitter.configs").setup {
+  require("nvim-treesitter.configs").setup({
     ensure_installed = { "lua", "markdown", "markdown_inline", "bash", "python" },
     highlight = { enable = true },
     indent = { enable = true },
     auto_install = true,
     sync_install = false,
     ignore_install = {},
-  }
+    textobjects = {
+      move = {
+        enable = true,
+        goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
+        goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+        goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
+        goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
+      },
+    },
+  })
 end
 
 return M
