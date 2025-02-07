232920a5a91cd98234cf3a3a8ee2305a67226594
Ariaszzzhc <ariaszzzhc@outlook.com>
2025-02-07 18:45:51 +0800

    remove python dap support

--- lua/plugins/daps/delve.lua
+++ lua/plugins/daps/delve.lua
@@ -4,10 +4,13 @@ return {
     local dap = require("dap")
 
     dap.adapters.delve = function(callback, config)
+      ---@diagnostic disable-next-line: undefined-field
       if config.mode == "remote" and config.request == "attach" then
         callback({
           type = "server",
+          ---@diagnostic disable-next-line: undefined-field
           host = config.host or "127.0.0.1",
+          ---@diagnostic disable-next-line: undefined-field
           port = config.port or "38697",
         })
       else
--- lua/plugins/lang/python.lua
+++ lua/plugins/lang/python.lua
@@ -13,29 +13,4 @@ return {
       },
     },
   },
-  {
-    "mfussenegger/nvim-dap",
-    opts = function()
-      local dap = require("dap")
-
-      dap.configurations.python = {
-        {
-          type = "python", -- the type here established the link to the adapter definition: `dap.adapters.python`
-          request = "launch",
-          name = "Launch file",
-          program = "${file}", -- This configuration will launch the current file if used.
-          pythonPath = function()
-            local cwd = vim.fn.getcwd()
-            if vim.fn.executable(cwd .. "/venv/bin/python") == 1 then
-              return cwd .. "/venv/bin/python"
-            elseif vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
-              return cwd .. "/.venv/bin/python"
-            else
-              return "/usr/bin/python"
-            end
-          end,
-        },
-      }
-    end,
-  },
 }
