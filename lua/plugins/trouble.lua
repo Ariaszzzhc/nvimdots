f63d1064d554583bc3b51e2c1cc6db422295e21f
Ariaszzzhc <ariaszzzhc@outlook.com>
2025-03-04 11:18:37 +0800

    update trouble config

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,43 +1,65 @@
 local M = {
-  "folke/trouble.nvim",
-  cmd = "Trouble",
-  cond = not vim.g.vscode,
-  keys = {
-    { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>",              desc = "Diagnostics" },
-    { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics" },
-    { "<leader>cs", "<cmd>Trouble symbols toggle<cr>",                  desc = "Symbols" },
-    { "<leader>cS", "<cmd>Trouble lsp toggle<cr>",                      desc = "LSP references/definitions/..." },
-    { "<leader>xl", "<cmd>Trouble loclist toggle<cr>",                  desc = "Location List" },
-    { "<leader>xq", "<cmd>Trouble qflist toggle<cr>",                   desc = "Quickfix List" },
-    {
-      "[q",
-      function()
-        if require("trouble").is_open() then
-          require("trouble").prev({ skip_groups = true, jump = true })
-        else
-          local ok, err = pcall(vim.cmd.cprev)
-          if not ok then
-            vim.notify(err, vim.log.levels.ERROR)
+  {
+    "folke/trouble.nvim",
+    cmd = "Trouble",
+    cond = not vim.g.vscode,
+    keys = {
+      { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics" },
+      { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics" },
+      { "<leader>cs", "<cmd>Trouble symbols toggle<cr>", desc = "Symbols" },
+      { "<leader>cS", "<cmd>Trouble lsp toggle<cr>", desc = "LSP references/definitions/..." },
+      { "<leader>xl", "<cmd>Trouble loclist toggle<cr>", desc = "Location List" },
+      { "<leader>xq", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List" },
+      {
+        "[q",
+        function()
+          if require("trouble").is_open() then
+            require("trouble").prev({ skip_groups = true, jump = true })
+          else
+            local ok, err = pcall(vim.cmd.cprev)
+            if not ok then
+              vim.notify(err, vim.log.levels.ERROR)
+            end
           end
-        end
-      end,
-      desc = "Previous Trouble/Quickfix Item",
-    },
-    {
-      "]q",
-      function()
-        if require("trouble").is_open() then
-          require("trouble").next({ skip_groups = true, jump = true })
-        else
-          local ok, err = pcall(vim.cmd.cnext)
-          if not ok then
-            vim.notify(err, vim.log.levels.ERROR)
+        end,
+        desc = "Previous Trouble/Quickfix Item",
+      },
+      {
+        "]q",
+        function()
+          if require("trouble").is_open() then
+            require("trouble").next({ skip_groups = true, jump = true })
+          else
+            local ok, err = pcall(vim.cmd.cnext)
+            if not ok then
+              vim.notify(err, vim.log.levels.ERROR)
+            end
           end
-        end
-      end,
-      desc = "Next Trouble/Quickfix Item",
+        end,
+        desc = "Next Trouble/Quickfix Item",
+      },
     },
-  }
+  },
+  {
+    "folke/snacks.nvim",
+    opts = function(_, opts)
+      return vim.tbl_deep_extend("force", opts or {}, {
+        picker = {
+          actions = require("trouble.sources.snacks").actions,
+          win = {
+            input = {
+              keys = {
+                ["<c-t>"] = {
+                  "trouble_open",
+                  mode = { "n", "i" },
+                },
+              },
+            },
+          },
+        },
+      })
+    end,
+  },
 }
 
 function M.config()
